2ef7b4bd8997cd565688af7e0d2e9ae599889208
Wilson-Wilson <j.a.wilson96@hotmail.com>
2017-02-04 18:26:43 -0500

    problem was in changed outlet name, fix via storyboard

--- timeTable/timeTable/ViewController.swift
+++ timeTable/timeTable/ViewController.swift
@@ -10,24 +10,27 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var sliderChange: UISlider!
     @IBOutlet weak var table: UITableView!
     
     
     @IBAction func sliderTouch(_ sender: Any) {
-        if slider.value > 0 && slider.value == slider.value.rounded() { table.reloadData() }
+        table.reloadData()
     }
     
-    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { return 50 }
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
     
     internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
         
-        cell.textLabel?.text = String((indexPath.row + 1) * Int(slider.value.rounded()))
+        cell.textLabel?.text = String((indexPath.row + 1) * Int(sliderChange.value * 20))
         
         return cell
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
